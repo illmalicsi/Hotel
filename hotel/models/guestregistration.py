d6d0c3cd6768629a1e6ece0ffe5b31e0a8eb53ab
Ivan Louie Malicsi <143686935+illmalicsi@users.noreply.github.com>
2025-04-28 13:06:01 +0800

    Add files via upload

--- hotel/models/guestregistration.py
+++ hotel/models/guestregistration.py
@@ -1,27 +1,16 @@
-# -*- coding: utf-8 -*-
-
-#guestregistration.py
-
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
-class guestregistration(models.Model):
+class GuestRegistration(models.Model):
     _name = 'hotel.guestregistration'
-    _description = 'hotel guest registration list'
+    _description = 'Hotel Guest Registration List'
     
     room_id = fields.Many2one("hotel.rooms", string="Room No.")
     guest_id = fields.Many2one("hotel.guests", string="Guest Name")
     
-    #roomname -< related fields found in the model hotel.rooms  
-    roomname=fields.Char("Room No.",related='room_id.name')
-    
-    #roomtname <- room type name found in the model hotel.rooms 
-    # also related to hotel.roomtypes
-    roomtname=fields.Char("Room Type",related='room_id.roomtypename')
-    
-    #guestname <- related field found as a computed field called name in 
-    # the model hotel.guests
-    guestname=fields.Char("Guest Name",related='guest_id.name')
+    roomname = fields.Char("Room No.", related='room_id.name')
+    roomtname = fields.Char("Room Type", related='room_id.roomtypename')
+    guestname = fields.Char("Guest Name", related='guest_id.name')
 
     datecreated = fields.Date("Date Created", default=lambda self: fields.Date.today())
     datefromSched = fields.Date("Scheduled Check In")
@@ -30,89 +19,65 @@ class guestregistration(models.Model):
     datetoAct = fields.Date("Actual Check Out")
    
     state = fields.Selection([
-        ('DRAFT','Draft'),
-        ('RESERVED','Reserved'),
-        ('CHECKEDIN','Checked In'), 
-        ('CHECKEDOUT','Checked Out'),                
-        ('CANCELLED','Cancelled')],
-        string="Status", default="DRAFT")
+        ('DRAFT', 'Draft'),
+        ('RESERVED', 'Reserved'),
+        ('CHECKEDIN', 'Checked In'), 
+        ('CHECKEDOUT', 'Checked Out'),                
+        ('CANCELLED', 'Cancelled')
+    ], string="Status", default="DRAFT")
    
-    name= fields.Char("Guest Registration",compute='_compute_name',store=False)  
+    name = fields.Char("Guest Registration", compute='_compute_name', store=False)
+
     @api.depends('room_id', 'guest_id')
     def _compute_name(self):
         for rec in self:
-            rec.name= f"{rec.roomname}, {rec.guestname}"
-         
-    def action_reserve(self):
+            rec.name = f"{rec.roomname}, {rec.guestname}"
+    
+    def _validate_reservation_fields(self):
         for rec in self:
-            if not(rec.guest_id):  
+            if not rec.guest_id:
                 raise ValidationError('Please supply a valid guest.')            
-           
-            elif not(rec.roomname):    
+            if not rec.roomname:
                 raise ValidationError('Please supply a valid Room Number.')    
-            elif not(rec.datefromSched):    
+            if not rec.datefromSched:
                 raise ValidationError('Please supply a valid Date From Schedule.')          
-            elif not(rec.datetoSched):    
+            if not rec.datetoSched:
                 raise ValidationError('Please supply a valid Date To Schedule.')          
-            elif (rec.datetoSched<rec.datefromSched):    
-                raise ValidationError('Invalid Date Range.')          
-            else:
-                #get the primary key value of guest registration
-                pkid = rec.id
-                #call the postgresql function the check for conflicts
-                self._cr.execute("select * from public.fncheck_registrationconflict("+str(pkid)+")")
-                result = self._cr.fetchall()
-                
-                #gather the query results
-                result_cnt = result[0][0]
-                result_msg = result[0][1]
-                
-                if (result_cnt==0):
-                    rec.state= "RESERVED"
-                else:
-                    raise ValidationError(result_msg)
-                               
-    def action_checkin(self):
-        for rec in self:
-            if not(rec.guest_id):  
-                raise ValidationError('Please supply a valid guest.')            
-            
-            elif not(rec.roomname):    
-                raise ValidationError('Please supply a valid Room Number.')            
-            elif not(rec.datefromSched):    
-                raise ValidationError('Please supply a valid Date From Schedule.')          
-            elif not(rec.datetoSched):    
-                raise ValidationError('Please supply a valid Date To Schedule.')          
-            elif (rec.datetoSched<rec.datefromSched):    
-                raise ValidationError('Invalid Date Range.')          
+            if rec.datetoSched < rec.datefromSched:
+                raise ValidationError('Invalid Date Range.')
 
+    def action_reserve(self):
+        self._validate_reservation_fields()
+        for rec in self:
+            pkid = rec.id
+            self._cr.execute("SELECT * FROM public.fncheck_registrationconflict(%s)", (pkid,))
+            result_cnt, result_msg = self._cr.fetchone()
+            if result_cnt == 0:
+                rec.state = "RESERVED"
             else:
-                #get the primary key value of guest registration
-                pkid = rec.id
-                
-                #call the postgresql function the check for conflicts
-                self._cr.execute("select * from public.fncheck_registrationconflict("+str(pkid)+")")
-                result = self._cr.fetchall()
-                
-                #gather the query results
-                result_cnt = result[0][0]
-                result_msg = result[0][1]
-                
-                if (result_cnt==0):
-                    rec.state= "CHECKEDIN"
-                else:
-                    raise ValidationError(result_msg)
-         
+                raise ValidationError(result_msg)
+
+    def action_checkin(self):
+        self._validate_reservation_fields()
+        for rec in self:
+            pkid = rec.id
+            self._cr.execute("SELECT * FROM public.fncheck_registrationconflict(%s)", (pkid,))
+            result_cnt, result_msg = self._cr.fetchone()
+            if result_cnt == 0:
+                rec.state = "CHECKEDIN"
+            else:
+                raise ValidationError(result_msg)
+
     def action_checkout(self):
-       for rec in self:
-         if (rec.state=="CHECKEDIN"):  
-            rec.state= "CHECKEDOUT"
-         else:                     
-            raise ValidationError("Guest is not CHECKED IN.") 
+        for rec in self:
+            if rec.state == "CHECKEDIN":
+                rec.state = "CHECKEDOUT"
+            else:
+                raise ValidationError("Guest is not CHECKED IN.") 
 
     def action_cancel(self):
-       for rec in self:
-         if (rec.state=="CHECKEDIN"):  
-            raise ValidationError("Guest has already CHECKED IN.")           
-         else:                     
-            rec.state= "CANCELLED"
+        for rec in self:
+            if rec.state == "CHECKEDIN":
+                raise ValidationError("Guest has already CHECKED IN.")           
+            else:
+                rec.state = "CANCELLED"
